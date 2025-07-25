53fd7b7259257bb6f828a3518782f76a8b8215e5
Nicholas Rockwell <avatar.nick@gmail.com>
2025-07-24 18:30:01 -0700

    Fix build: exclude amplify directory from frontend compilation and remove deprecated appDir option

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['*.amazonaws.com'],
+  },
+  webpack: (config, { isServer }) => {
+    // Exclude the amplify directory from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/amplify/**'],
+    };
+    
+    // Exclude amplify directory from module resolution
+    config.resolve.modules = config.resolve.modules || [];
+    config.resolve.modules.push('node_modules');
+    
+    return config;
+  },
+  // Exclude amplify directory from TypeScript compilation
+  typescript: {
+    ignoreBuildErrors: false,
   },
 }
 
