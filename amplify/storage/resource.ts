6ed034281e4ec64f6a7135891102a198a84e4679
Nicholas Rockwell <avatar.nick@gmail.com>
2025-07-25 13:25:10 -0700

    fix: change storage name to comply with AWS naming rules - replace 12days with twelvedays

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: '12days-media-storage',
+  name: 'twelvedays-media-storage',
   access: (allow) => ({
     'media/{entity_id}/*': [
       allow.authenticated.to(['read', 'write']),
